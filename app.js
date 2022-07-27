fd701ab1880683f2fa010510864f4bb1a79f0b33
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-27 16:29:14 -0400

    validate answers

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log('Please enter your github username!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
@@ -37,12 +53,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: projectNameInput => {
+            if (projectNameInput) {
+              return true;
+            } else {
+              console.log('Please enter your project name!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: describeProjectInput => {
+            if (describeProjectInput) {
+              return true;
+            } else {
+              console.log('Please enter your project description!');
+              return false;
+            }
+          }
       },
       {
         type: 'checkbox',
@@ -53,7 +85,15 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: githubLinkInput => {
+            if (githubLinkInput) {
+              return true;
+            } else {
+              console.log('Please enter your github project link!');
+              return false;
+            }
+          }
       },
       {
         type: 'confirm',
