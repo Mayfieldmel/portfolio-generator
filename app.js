4e53877f4524b0a865a30ce93b39987c00b0c913
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-07-29 12:13:10 -0400

    modularize finished site

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site.js');
 
--- utils/generate-site.js
+++ utils/generate-site.js
@@ -19,7 +19,7 @@ const writeFile = fileContent => {
     });
   };
 
-  const copyFile = fileContent => {
+  const copyFile = () => {
     return new Promise((resolve, reject) => {
         fs.copyFile('./src/style.css', './dist/style.css', err => {
             if (err) {
